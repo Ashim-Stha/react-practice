45bd23540abf98547d1df16a6cadcdd984e6f18f
Ashim-Stha <ashimshrestha2384@gmail.com>
2023-01-06 16:56:16 +0545

    react3

--- src/components/Form.js
+++ src/components/Form.js
@@ -15,20 +15,28 @@ export default function Form(props) {
 
     }
     const reset = () =>{
-        setText('Enter your text here');
+        setText('');
+    }
+    const copy = ()=>{
+      let vartext = document.getElementById('inputPassword5');
+      vartext.select();
+      navigator.clipboard.writeText(vartext.value);
     }
   return (
     <>
     <div className='container'>
       <label htmlFor="inputPassword5" className="form-label"><b>{props.heading}</b></label>
 <input type="text" id="inputPassword5" rows="8" value={text} onChange={onChangehandle} className="form-control" aria-describedby="passwordHelpBlock"/>
-<button className="btn btn-primary" onClick={changeUppercase}>Convert to uppercase</button>
-<button className='btn btn-primary mx-2' type="reset" onClick={reset}>Reset</button>
+<button disabled={text.length===0} className="btn btn-primary" onClick={changeUppercase}>Convert to uppercase</button>
+<button disabled={text.length===0} className='btn btn-primary mx-2' type="reset" onClick={reset}>Reset</button>
+<button disabled={text.length===0} className="btn btn-primary mx-2" onClick={copy}>Copy</button>
 
     </div>
-    <div className="cotainer my-3">
-        {text.split(" ").length} words and {text.length} characters
-        <div>{text.split(" ").length * 0.008} minutes to read</div>
+    <div className="container my-3">
+        {text.split(/\s+/).filter((element)=>{
+          return element.length !== 0;
+        }).length} words and {text.length} characters
+        <div>{text.split(/\s+/).filter((element)=>{return element.length!==0}).length * 0.008} minutes to read</div>
     </div>
     </>
   )
